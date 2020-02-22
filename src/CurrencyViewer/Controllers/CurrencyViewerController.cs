41900b13ba7e6b948b75eac02d64dd72c1b5d86e
Damian N <dnaprawa@gmail.com>
2020-02-22 07:36:08 +0100

    Added projects

--- src/CurrencyViewer/Controllers/CurrencyViewerController.cs
+++ src/CurrencyViewer/Controllers/CurrencyViewerController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CurrencyViewer.Controllers
+﻿namespace CurrencyViewer.API.Controllers
 {
     public class CurrencyViewerController
     {
--- src/CurrencyViewer/Startup.cs
+++ src/CurrencyViewer/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace CurrencyViewer
+namespace CurrencyViewer.API
 {
     public class Startup
     {
